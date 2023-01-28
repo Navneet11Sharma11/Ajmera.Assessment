c03e6ccd6eaeb5ec3412a71c1f260286585fd000
Navneet Sharma <navneet11kumar11@hotmail.com>
2023-01-29 03:42:23 +0530

    Changed in the configuration code

--- Ajmera.Assessment.DL/Models/AjmeraContext.cs
+++ Ajmera.Assessment.DL/Models/AjmeraContext.cs
@@ -19,7 +19,7 @@ public partial class AjmeraContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost; Persist Security Info=True; Initial Catalog=Ajmera; Integrated Security=SSPI;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=.;Database=Ajmera;User ID=SA; Password=Password123;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
